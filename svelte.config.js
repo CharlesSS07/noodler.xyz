08d069b8e7c4188b1bc37eadbf1e1b6df8293616
cs <charles.s.strauss@gmail.com>
2025-06-07 19:05:07 -0600

    fixed discrepancy between build and dev not using html suffix

--- svelte.config.js
+++ svelte.config.js
@@ -24,13 +24,11 @@ export default {
     preprocess: vitePreprocess(),
     kit: {
         adapter: adapter({
-            // default options are shown. On some platforms
-            // these options are set automatically — see below
             pages: 'build',
             assets: 'build',
-            fallback: 'build',
+            fallback: 'index.html',
             precompress: false,
-            strict: true,
+            strict: false,
         }),
         files: {
             // Exclude examplesAndDocs from being processed by SvelteKit
